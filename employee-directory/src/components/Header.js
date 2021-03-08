e6fdfda7cd1da295679727d114127ba3f8e33127
David Rojo <rojo@rojorevolution.com>
2021-03-08 16:35:32 -0600

    set up initial React App, gotthe header rendering to the page so far

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header className="container-flex bg-primary">
+            <div className="container p-5">
+                <h1 className="text-light text-center">Employee Directory</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ employee-directory/src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+
+function MainContainer() {
+    return (
+        <Header />
+    );
+
+}
+
+
+
+export default MainContainer;
