f69bde5eef1dded52a58f3ff9e885ee89db5ac9c
David Rojo <rojo@rojorevolution.com>
2021-03-09 23:12:21 -0600

    minor style tweaks

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
     return (
-        <header className="container-flex bg-primary">
+        <header className="container-flex headerbg">
             <div className="container p-5">
                 <h1 className="text-light text-center">Employee Directory</h1>
             </div>
--- employee-directory/src/components/SearchBar.js
+++ employee-directory/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function SearchBar(props) {
     return (
         <div className="container-flex">
-            <div className="container d-flex justify-content-center my-4">
+            <div className="container d-flex justify-content-center my-5">
                 <form className="g-3 form-width" onSubmit={props.handleFormSubmit}>
                     <input
                         className="form-control"
@@ -13,7 +13,6 @@ function SearchBar(props) {
                         onChange={props.handleInputChange}
                         // value={props.search}
                         id="search" />
-                    <button type="submit" className="btn btn-primary my-2 w-100">Search</button>
                 </form>
             </div>
         </div>
