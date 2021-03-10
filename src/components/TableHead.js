e16395f6e90d7944ae3344adf15569354094814d
David Rojo <rojo@rojorevolution.com>
2021-03-10 15:26:56 -0600

    text changes to the table header

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -5,11 +5,11 @@ function TableHead(props) {
     return (
         <thead >
             <tr className="bg-white" >
-                <td className="ms-2 p-2">Image</td>
-                <td className="ms-2 p-2">Name <i class="fas fa-sort cursor" onClick={props.sortByFirstName}></i>
+                <td className="ms-2 p-2"></td>
+                <td className="ms-2 p-2">Name: <i class="fas fa-sort cursor" onClick={props.sortByFirstName}></i>
                 </td>
-                <td className="ms-2 p-2">Phone</td>
-                <td className="ms-2 p-2">Email</td>
+                <td className="ms-2 p-2">Email:</td>
+                <td className="ms-2 p-2">Phone:</td>
             </tr>
         </thead >
     )
