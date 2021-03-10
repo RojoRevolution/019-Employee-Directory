846e2aa4490cbd55abde2d231cfc4fac85b157c3
David Rojo <rojo@rojorevolution.com>
2021-03-09 22:57:47 -0600

    added TableHead component

--- employee-directory/src/components/ResultContainer.js
+++ employee-directory/src/components/ResultContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import EmployeeTable from "./EmployeeTable";
+import TableHead from "./TableHead";
 import SearchBar from "./SearchBar";
 import API from "../utils/API";
 
@@ -58,22 +59,13 @@ class ResultsContainer extends Component {
         this.setState({ results: sortedEmployees })
     }
 
-
-
     render() {
         return (
             <div className="container-flex">
                 <div className="container">
                     <SearchBar search={this.state.search} handleInputChange={this.handleInputChange} handleFormSubmit={this.handleFormSubmit} />
                     <table>
-                        <thead >
-                            <tr className="bg-white" >
-                                <td className="ms-2 p-2">Image</td>
-                                <td className="ms-2 p-2">Name </td>
-                                <td className="ms-2 p-2">Phone</td>
-                                <td className="ms-2 p-2">Email</td>
-                            </tr>
-                        </thead>
+                        <TableHead />
                         <EmployeeTable results={this.state.results} filter={this.state.search} />
 
                     </table>
