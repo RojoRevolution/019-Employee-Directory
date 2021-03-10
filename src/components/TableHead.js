d0e60f4c6927f0bbf401824053c12710aba9b4d9
David Rojo <rojo@rojorevolution.com>
2021-03-10 15:08:56 -0600

    small style changes

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -6,7 +6,7 @@ function TableHead(props) {
         <thead >
             <tr className="bg-white" >
                 <td className="ms-2 p-2">Image</td>
-                <td className="ms-2 p-2">Name <i class="fas fa-sort" onClick={props.sortByFirstName}></i>
+                <td className="ms-2 p-2">Name <i class="fas fa-sort cursor" onClick={props.sortByFirstName}></i>
                 </td>
                 <td className="ms-2 p-2">Phone</td>
                 <td className="ms-2 p-2">Email</td>
